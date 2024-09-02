8adfe69396811ce223250e4c3cb84a7fdadaec3d
Globalinvestors94 <166951378+Globalinvestors94@users.noreply.github.com>
2024-09-02 15:32:43 +0100

    Update models.py

--- hk/models.py
+++ hk/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
+class ScreenRecording(models.Model):
+	video_file = models.FileField(upload_to='recordings/')
+	uploaded_at = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return f"Recording {self.id} uploaded at {self.uploaded_at}"
+
 # Create your models here.
